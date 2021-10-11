3ca0264eca47aca1a3185fb496ef64a0610b6a3b
amr alaa yassen <amralaa566@gmail.com>
2021-10-11 17:02:28 +0200

    fix path in loadLinuxScript

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call (Map config=[:]) {
-    def scriptcontents = libraryResource "org/scripts/linux${config.scriptName}"
+    def scriptcontents = libraryResource "org/scripts/linux/${config.scriptName}"
     writefile file:"${config.scriptName}", text: scriptcontents
     sh "chmod a+x ./${config.scriptName}"
 }
